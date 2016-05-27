96c0482c606af8e93930911cff70bf76065d6e44
sooraj.v <soorajv.c@ivedix.com>
2016-05-27 13:00:18 +0530

    Add Event Handler

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -11,6 +11,12 @@ namespace HelloWorld
         public MainWindow()
         {
             InitializeComponent();
+            ClickMeButton.Click += ClickMeButton_Click;
+        }
+
+        void ClickMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hellow World");
         }
     }
 }
